fee028d2198e4bcbb95afc28704cad5036b0d343
Ankur <ankur.gupta.cd.eee19@itbhu.ac.in>
2022-09-05 01:20:28 +0530

    Built the production build

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import path from 'path'
+import fs from 'fs'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -15,10 +17,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  response.send('API is running....')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -27,9 +25,28 @@ app.use('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+if (process.env.NODE_ENV == 'production') {
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      fs.readFileSync(path.join(__dirname, '/frontend/build/index.html'))
+    )
+  })
+} else {
+  app.get('/', (request, response) => {
+    response.send('API is running....')
+  })
+}
+
 app.use(notFound)
 
 app.use(errorHandler)
+// app.get('/', (req, res) => {
+//   res.send('API is running..')
+//   res.end()
+// })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
